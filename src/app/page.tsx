58fa020bf9e5952733c0e4fae266e642705c7b08
ImDhairya <98574278+ImDhairya@users.noreply.github.com>
2024-06-08 16:06:24 +0530

    added dotted grid bg

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 import HeroSection from "./components/HeroSection";
 import FeaturedCourses from "./components/FeaturedCourses";
 import WhyChooseUs from "./components/whyChooseUs";
+import MusicSchoolTestimonials from "./components/TestimonialCards";
 
 export default function Home() {
   return (
@@ -10,6 +11,7 @@ export default function Home() {
       <HeroSection />
       <FeaturedCourses />
       <WhyChooseUs />
+      <MusicSchoolTestimonials/>
     </main>
   );
 }
