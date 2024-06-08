a2db4746f8c0b109a5002667188eb8d78465b822
ImDhairya <98574278+ImDhairya@users.noreply.github.com>
2024-06-08 11:54:45 +0530

    added WhyChooseUs file and also added ui infinite moving file

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,15 @@
 import Image from "next/image";
 import HeroSection from "./components/HeroSection";
+import FeaturedCourses from "./components/FeaturedCourses";
+import WhyChooseUs from "./components/whyChooseUs";
 
 export default function Home() {
   return (
     <main className=" min-h-screen bg-black/[0.96] bg-grid-white/[0.02] antialiased">
       {/* <h1 className=" text-2xl text-center"> Chai aur code</h1> */}
-      <HeroSection/>
+      <HeroSection />
+      <FeaturedCourses />
+      <WhyChooseUs />
     </main>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,6 @@
 import type {Config} from "tailwindcss";
 const colors = require("tailwindcss/colors");
+const defaultTheme = require("tailwindcss/defaultTheme");
 const {
   default: flattenColorPalette,
 } = require("tailwindcss/lib/util/flattenColorPalette");
@@ -32,8 +33,15 @@ const config: Config = {
       },
       animation: {
         spotlight: "spotlight 2s ease .75s 1 forwards",
+        scroll:
+          "scroll var(--animation-duration, 40s) var(--animation-direction, forwards) linear infinite",
       },
       keyframes: {
+        scroll: {
+          to: {
+            transform: "translate(calc(-50% - 0.5rem))",
+          },
+        },
         spotlight: {
           "0%": {
             opacity: "0",
